107725095bf56deca4323b2196e5c667fae4a10c
James-Dailey <101994736+James-Dailey@users.noreply.github.com>
2022-08-26 11:02:18 -0500

    question

--- hello-world.py
+++ hello-world.py
@@ -1,2 +1,4 @@
-print('Hello world!')
+#  print('Hello world!')#
 #  add more to the print statement, comment and commit the changes you make, then push them to the repo#
+
+print('Would you like to play a game?')
