a8a25c4278c2c0d41a324e59b9df5f6cf5a28286
HollymRobinson <Hladymarie@gmail.com>
2022-08-28 11:42:53 -0500

    What game should we create?
    In college I made a dice game, I have to open my zip file to confirm. But I am aware that people also make card games or fill-in-the-blank games. All are fine with me! Should we add players? Bobby and Aurelie follows me on Github but neither have reached out to me yet.

--- hello-world.py
+++ hello-world.py
@@ -1,4 +1,9 @@
 #  print('Hello world!')#
 #  add more to the print statement, comment and commit the changes you make, then push them to the repo#
 
-print('Would you like to play a game?')
+print('Would you like to play a game? ')
+answer = input("Enter yes or no: ")
+if answer == "yes":
+    print("What game should we play?: ")
+#what game should we play and how many players can play?
+
